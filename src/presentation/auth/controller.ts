a601459064a8b712acaeee07fa1f6b1603c80fe2
Alonso <marenas@itsmarts.com.mx>
2024-06-25 12:16:01 -0600

    error added

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express"
+
+
+
+export class AuthController {
+
+    //DI
+    contructor(
+
+    ){}
+
+    registerUser = ( req: Request, res: Response) => {
+
+        res.json('registerUser')
+    }
+
+    
+    loginUser = ( req: Request, res: Response) => {
+
+        res.json('loginUser')
+    }
+
+    
+    validateEmail = ( req: Request, res: Response) => {
+
+        res.json('validateEmail')
+    }
+}
